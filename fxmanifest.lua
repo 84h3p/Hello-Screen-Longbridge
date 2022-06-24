4e7c7b661fe26bf86acdd60b29445c0da621d3f6
84h3p <vano0448@gmail.com>
2022-06-24 18:44:40 +0600

    second page added

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+ui_page 'index.html'
+
+-- every client-side file still needs to be added to the resource packfile!
+files {
+    'client.lua'
+    'index.html',
+    'script.js',
+    'style.css',
+    'assets/background.png'
+}
--- script.js
+++ script.js
@@ -1,8 +1,43 @@
 const content = document.getElementById('content');
 const nextButton = document.getElementById('nextButton')
+const header = document.getElementById('header')
 
 function next(event) {
-  content.innerHTML = '';
+  content.innerHTML = `
+
+
+
+
+  <div id="textInfo">
+    Здесь вы можете играть в следующих департаментах:
+  </div>
+
+  <div class="wrapper">
+    <div>
+      <h2> LSPD </h2> хранители городских улиц
+    </div>
+    <div>
+      <h2> BCSO </h2> защитники округа
+    </div>
+    <div>
+      <h2> SAHP </h2> контроль шоссе
+    </div>
+    <div>
+      <h2> Civill Department </h2> жители Лос-Сантоса
+    </div>
+    <div>
+      <h2> DPS </h2> безопасность улиц
+    </div>
+    <div>
+      <h2> Dispatch Department </h2> координация
+    </div>
+  </div>
+
+
+
+<button id="nextButton" name="button"> Далее </button>
+  `;
+  header.innerHTML = ''
 
 }
 
